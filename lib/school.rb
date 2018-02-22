24bd643c0c96407595899067a912ed0eccd6d585
alec-horwitz <horwitz_alec@wheatoncollege.edu>
2018-02-22 14:27:01 -0500

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,21 @@
 # code here!
+class School
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+  attr_reader :roster
+  def add_student(name, grade)
+    self.roster.include?(grade) ? self.roster[grade].push(name) : self.roster[grade] = [name]
+  end
+
+  def grade(grade)
+    self.roster[grade]
+  end
+
+  def sort
+    self.roster.each { |grade, students|
+      self.roster[grade] = students.sort
+    }
+  end
+end
